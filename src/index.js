aac8a7455f06975d827a7e51c086a90e3d8bd85e
unknown <aimen.louafi@mines-paristech.fr>
2019-12-08 22:42:16 +0100

    changed font

--- src/index.js
+++ src/index.js
@@ -6,6 +6,13 @@ import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'typeface-roboto'
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Raleway:300,400,700', 'sans-serif']
+  }
+});
 
 ReactDOM.render(<App/>, document.getElementById('root'));
 registerServiceWorker();
