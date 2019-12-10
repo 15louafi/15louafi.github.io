c359931d2fdfbcbd808e288a26919f51d36da7d9
unknown <aimen.louafi@mines-paristech.fr>
2019-12-10 21:57:07 +0100

    Renewal of footer (no longer sticky)

--- src/commonComponents/Footer.js
+++ src/commonComponents/Footer.js
@@ -5,42 +5,31 @@ const footerStyle = {
   backgroundColor: "white",
   fontSize: "15px",
   color: "black",
-  borderTop: "1px solid #0a07c2",
   textAlign: "center",
-  padding: "15px",
-  position: "fixed",
   left: "0",
   bottom: "0",
-  height: "50px",
   width: "100%"
 };
 
-const phantomStyle = {
-  display: "block",
-  padding: "20px",
-  height: "60px",
-  width: "100%"
-};
 
 function Footer() {
   return (
     <div>
-      <div style={phantomStyle} />
       <div style={footerStyle}> <div>
                 <div className="row"> 
             <div className="contact col-3">
-              <a href="mailto:aimen.louafi@shift-technology.com" target="_blank"><span className="contactIcon text-center"><i className="fas fa-envelope"/></span>
+              <a href="mailto:aimen.louafi@shift-technology.com" target="_blank"><span className="contactIcon text-center"><i className="fas fa-envelope"/><span className="contactText body">Email</span></span>
               </a>
             </div>
             <div className="contact col-3">
-              <a href="https://github.com/15louafi" target="_blank"><span className="contactIcon"><i className="fab fa-github"/></span>
+              <a href="https://github.com/15louafi" target="_blank"><span className="contactIcon"><i className="fab fa-github"/><span className="contactText body">Github</span></span>
               </a>
             </div>
              <div className="contact col-3">
-              <a href="https://www.linkedin.com/in/aïmen-louafi-164868116/" target="_blank"><span className="contactIcon text-center"><i className="fab fa-linkedin"/></span>
+              <a href="https://www.linkedin.com/in/aïmen-louafi-164868116/" target="_blank"><span className="contactIcon text-center"><i className="fab fa-linkedin"/><span className="contactText body">LinkedIn</span></span>
               </a>
             </div>
-            <div className="contact col-3 body greyText">
+            <div className="contact col-3 body copyright">
               &#9400; Aimen Louafi - Powered by React
             </div>
           </div>
